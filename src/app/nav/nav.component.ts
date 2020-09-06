3c1276b78ba7482c82a3c7c20a248b3ee16bf48f
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-06 22:40:09 +0700

    them chuc nang login, logout trong nav

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../service/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged is successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  // tslint:disable-next-line:typedef
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+}
