addb67c8d72ff22ca2155e2fc9b9cfbdf8ae3994
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-07 15:12:03 +0700

    cai dat auth0/angular-jwt

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -26,8 +26,9 @@ export class NavComponent implements OnInit {
 
   // tslint:disable-next-line:typedef
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    // const token = localStorage.getItem('token');
+    // return !!token;
+    return this.authService.loggedIn();
   }
 
   // tslint:disable-next-line:typedef
