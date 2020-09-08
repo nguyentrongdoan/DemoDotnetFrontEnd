d12967bc5b29d31c49dc12cec4ae6e98a08081f5
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-08 08:52:47 +0700

    Using routing in our components

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../service/auth.service';
 import {AlertifyService} from '../service/alertify.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,17 +11,20 @@ import {AlertifyService} from '../service/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
   // tslint:disable-next-line:typedef
   login() {
-    this.authService.login(this.model).subscribe(() => {
+    this.authService.login(this.model).subscribe( () => {
       this.alertify.success('logged in successfully');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -35,5 +39,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
