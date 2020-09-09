7e02eeab4231009619aab7226e7f10d218c0cc6e
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-09 15:17:17 +0700

    Designing the template for the                                  member profile edit page - part 2

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {User} from '../../model/user';
+import {AlertifyService} from '../../service/alertify.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,14 +10,22 @@ import {User} from '../../model/user';
   styleUrls: ['./member-edit.component.scss']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private alertify: AlertifyService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
       this.user = data.user;
     });
+  }
+  // tslint:disable-next-line:typedef
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile update successfully');
+    this.editForm.reset(this.user);
   }
 
 }
