9e106733989fcb04f6b9cb06b9ae7ba6ef3f1abb
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-11 11:33:48 +0700

    Adding the Delete Photo functionality to the SPA

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -29,4 +29,8 @@ export class UserService {
   setMainPhoto(userId: number, id: number) {
     return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {});
   }
+  // tslint:disable-next-line:typedef
+  deletePhoto(userId: number, id: number) {
+    return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+  }
 }
