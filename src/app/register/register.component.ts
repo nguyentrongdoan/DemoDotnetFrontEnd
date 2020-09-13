58d483558604ae625fad63746fd3d9542532bf4b
nguyentrongdoan <nguyendoan206@gmail.com>
2020-09-13 15:40:52 +0700

    Expanding the Registration form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -28,7 +28,12 @@ export class RegisterComponent implements OnInit {
   // tslint:disable-next-line:typedef
   createRegisterForm() {
     this.registerForm = this.fb.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', Validators.required]
     }, {validators: this.passwordMatchValidator});
